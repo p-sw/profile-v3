ed0e7941563363639b01ac72a6b43509deb29dfc
sserve-kr <personal@sserve.me>
2023-06-17 12:07:29 +0900

    feat: add projects page template

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Root from "./Root";
-import Index from "@r/Index";
-import About from "@r/about";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./error";
-import theme from "./theme";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Root from './Root';
+import Index from '@r/Index';
+import About from '@r/about';
+import Projects from '@r/projects';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './error';
+import theme from './theme';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Index /> },
-      { path: "about", element: <About /> },
+      { path: 'about', element: <About /> },
+      { path: 'projects', element: <Projects /> },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
